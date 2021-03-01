e1f51a278ffb054615c88954df36f80e717bdd9c
GT <grzegorz.topolewski@wskz.pl>
2021-03-01 19:53:26 +0100

    create react app

--- module_1/poligon/src/App.test.js
+++ module_1/poligon/src/App.test.js
@@ -5,4 +5,14 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const helloElement = screen.getByText(/hello/i);
+  expect(helloElement).toBeInTheDocument();
+});
+
+test('render hello element', () => {
+  render(<App />);
+
+  const helloElement = screen.getByText(/hello/i);
+  expect(helloElement).toBeInTheDocument();
 });
