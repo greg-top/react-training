47b6ee9fa7c9f4335bc2d37b15d1bc488df43fbd
GT <grzegorz.topolewski@wskz.pl>
2021-03-06 16:44:43 +0100

    Dodanie poligon oraz zadania nr 1 z modulu 2

---
+++ module_2/clickbait/src/App.js
@@ -0,0 +1,42 @@
+import './clickbait.css';
+import './clickbait-flex.css';
+
+const posts = [
+  { id: 1, title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
+  { id: 2, title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
+  { id: 3, title: 'Adam Małysz Zgolił wąs',
+    intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy'
+  }
+]
+
+const cutIntro = (el, chars) => {
+  console.log(`el: ${el}`);
+  console.log(`chars: ${chars}`);
+  if (el.length > chars) {
+    return `${el.substring(0, chars)}...`;
+  } else {
+    return el;
+  }
+}
+
+function BlogTile({title, intro}) {
+  return (
+    <div className="card">
+      <span className="card__number"></span>
+      <span className="card__intro--cut">{cutIntro(intro, 15)}</span>
+      <span className="card__body">{intro}</span>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="clickbait">
+      {posts.map((post, index) => {
+        return <BlogTile title={post.title} intro={post.intro} key={post.id} />
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ module_2/poligon/src/App.js
@@ -0,0 +1,114 @@
+import React, {useState} from 'react';
+
+import './App.css';
+import './reset.css';
+import './normalize.css';
+
+const yellow = '#f39c12';
+
+const styles = {
+  backgroundColor: '#e74c3c',
+  border: '#3498db 1px solid',
+  padding: 30,
+  paragraph: {
+    backgroundColor: yellow
+  }
+}
+
+// //komponent klasowy
+// class News extends React.Component {
+
+//   //korzystanie ze state za pomocą kontruktora
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     score: 5
+//   //   }
+//   // }
+
+//   //konfiguracja state 
+//   state = {
+//     score: 6
+//   }
+
+//   //stan życia komponentu (omówione dalej w kursie)
+//   componentDidMount() {
+//     let loader = true;
+//     //zmiana state 
+//     this.setState({score: 8}, ()=> {
+//       loader = false;
+//     });
+//   }
+
+//   render() {
+
+//     const {heading, intro} = this.props;
+//     const {score} = this.state;
+
+//     return (
+//       <div className="news">
+//         <h2>{heading}</h2>
+//         <p>{intro}</p>
+//         <p>{score}</p>
+//       </div>
+//     );
+//   }
+// }
+
+//komponent funkcyjny
+// function News({heading, intro}) {
+//   return (
+    // <div className="news">
+    //   <h2>{heading}</h2>
+    //   <p>{intro}</p>
+    // </div>
+
+    // JSS - JavaScript in CSS
+    //   <div style={styles}>
+    //   <h2>{heading}</h2>
+    //   <p style={styles.paragraph}>{intro}</p>
+    // </div>
+//   );
+// }
+
+// komponent funkcyjny ze stanem (hooks)
+
+function News({heading, intro}) {
+  const [score, changeScore] = useState(11);
+
+  // changeScore(22); //złe miejsce do zmiany state, omówione w dalszej części kursu
+  return (
+    <div className="news">
+      <h2>{heading}</h2>
+      <p>{intro}</p>
+      <p>{score}</p>
+    </div>
+  );
+}
+
+const data = [
+  {
+    heading: "Nagłówek 1",
+    intro: "Intro 1"
+  },
+  {
+    heading: "Nagłówek 2",
+    intro: "Intro 2"
+  },  {
+    heading: "Nagłówek 3",
+    intro: "Intro 3"
+  }
+];
+
+
+function App() {
+  return (
+    <>
+      {data.map((el, index) => {
+        return <News heading={el.heading} intro={el.intro} key={`news-${index}`} />
+      })}
+    </>
+  );
+}
+
+export default App;
