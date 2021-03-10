b794f5e35a6b97ca41267843ef63695ea06efe95
greg-top <topolewski.gregory@gmail.com>
2021-03-10 19:55:28 +0100

    podzial plikow i katalogow - basic

--- module_3/poligon/src/App.js
+++ module_3/poligon/src/App.js
@@ -1,94 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
+
+import News from './components/News';
 
 import './App.css';
 import './reset.css';
 import './normalize.css';
 
-const yellow = '#f39c12';
-
-const styles = {
-  backgroundColor: '#e74c3c',
-  border: '#3498db 1px solid',
-  padding: 30,
-  paragraph: {
-    backgroundColor: yellow
-  }
-}
-
-// //komponent klasowy
-// class News extends React.Component {
-
-//   //korzystanie ze state za pomocą kontruktora
-//   // constructor(props) {
-//   //   super(props);
-//   //   this.state = {
-//   //     score: 5
-//   //   }
-//   // }
-
-//   //konfiguracja state 
-//   state = {
-//     score: 6
-//   }
-
-//   //stan życia komponentu (omówione dalej w kursie)
-//   componentDidMount() {
-//     let loader = true;
-//     //zmiana state 
-//     this.setState({score: 8}, ()=> {
-//       loader = false;
-//     });
-//   }
-
-//   render() {
-
-//     const {heading, intro} = this.props;
-//     const {score} = this.state;
-
-//     return (
-//       <div className="news">
-//         <h2>{heading}</h2>
-//         <p>{intro}</p>
-//         <p>{score}</p>
-//       </div>
-//     );
-//   }
-// }
-
-//komponent funkcyjny
-// function News({heading, intro}) {
-//   return (
-    // <div className="news">
-    //   <h2>{heading}</h2>
-    //   <p>{intro}</p>
-    // </div>
-
-    // JSS - JavaScript in CSS
-    //   <div style={styles}>
-    //   <h2>{heading}</h2>
-    //   <p style={styles.paragraph}>{intro}</p>
-    // </div>
-//   );
-// }
-
-// komponent funkcyjny ze stanem (hooks)
-
-function News({heading, intro}) {
-  const [score] = useState(11);
-
-  return (
-    <div className="news" style={styles}>
-      <h2>{heading}</h2>
-      <p style={styles.paragraph}>{intro}</p>
-      <p>Ocena: {score}</p>
-    </div>
-  );
-}
-
 const data = [
-  { id: 1, title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
-  { id: 2, title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
-  { id: 3, title: 'Adam Małysz Zgolił wąs',
+  { id: 1, author: 'Greg Topolewski', title: 'Pilne: Co to był za dzień!', intro: 'Tego świat jeszcze nie widział'},
+  { id: 2, author: 'Greg Topolewski', title: 'Wszyscy zazdroszą Polakom!', intro: 'Takiego clickbajtowego tytułu jeszcze nikt nie wymyślił'},
+  { id: 3, author: 'Greg Topolewski', title: 'Adam Małysz Zgolił wąs',
     intro: 'Po przegranym zakładzie z Piotrem Żyłą nasz mistrz olimpijski zgolił wąsy'
   }
 ];
@@ -98,7 +19,12 @@ function App() {
   return (
     <>
       {data.map((el) => {
-        return <News heading={el.title} intro={el.intro} key={`news-${el.id}`} />
+        return <News 
+        heading={el.title} 
+        intro={el.intro} 
+        author={el.author}
+        key={`news-${el.id}`
+      } />
       })}
     </>
   );
