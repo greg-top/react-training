1a6f96a14fc860eae4f79eb76bb00633bab48083
GT <grzegorz.topolewski@wskz.pl>
2021-03-19 13:10:59 +0100

    module 3 - homework - card - poprawki

--- module_3/homework/src/components/Card/CardCollapse.js
+++ module_3/homework/src/components/Card/CardCollapse.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function CardCollapse({ collapse }) {
     return (
-        <div className="card__collapse hidden">
+        <div className="card__collapse">
             <p>{collapse}</p>
         </div>
     );
--- module_3/homework/src/components/Card/CardFooter.js
+++ module_3/homework/src/components/Card/CardFooter.js
@@ -5,9 +5,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const toggleCollapse = () => {
     const collapse = document.querySelector('.card__collapse');
     collapse.classList.toggle('show');
-    
+
     const icon = document.querySelector('.card__footer svg.fa-chevron-down');
-    icon.classList.toggle('rotate'); 
+    icon.classList.toggle('rotate');
 }
 
 function CardFooter({ showMore }) {
