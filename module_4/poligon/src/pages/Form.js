f22255f6a5449b4bc011baa45e501d3f5f549235
greg-top <topolewski.gregory@gmail.com>
2021-03-22 19:51:33 +0100

    module 4 - poligon - stworzenie wlasnego hooka i manipulacja useState

--- module_4/poligon/src/pages/Form.js
+++ module_4/poligon/src/pages/Form.js
@@ -1,40 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Form extends React.Component {
-    state = {
-        name: "",
-        surname: "",
-        age: ""
-    }
-    handleSubmit(e) {
+function useInput( initValue = "" ) {
+    const [value, setValue] = useState(initValue);
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    };
+
+    return [value, handleChange];
+}
+
+function Form() {
+    const [name, setName] = useInput();
+    // const [name, setName] = useInput("Jan");
+    const [surname, setSurname] = useInput();
+    const [age, setAge] = useInput();
+
+    const handleSubmit = (e) => {
         e.preventDefault();
     }
-    handleClick(e) {
-        console.log("React button");
-        console.log("handleClick:", e);
-    }
-    handleChange = (e) => {
-        const keyName = e.target.name;
-        const keyValue = e.target.value;
 
-        if(keyValue) this.setState({ [keyName] : e.target.value });
+    const handleClick = () => {
+        console.log("React button clicked");
     }
-    render() {
-        return (
-        <form onSubmit={this.handleSubmit}> 
+
+    return(
+        <form onSubmit={handleSubmit}> 
             <div>
-                <input name="name" type="text" placeholder="Name" onChange={this.handleChange} />
+                <input name="name" type="text" placeholder="Name" onChange={setName} />
             </div>
             <div>
-                <input name="surname" type="text" placeholder="Surname" onChange={this.handleChange} />
+                <input name="surname" type="text" placeholder="Surname" onChange={setSurname} />
             </div>
             <div>
-                <input name="age" type="text" placeholder="Age" onChange={this.handleChange} />
+                <input name="age" type="text" placeholder="Age" onChange={setAge} />
             </div>
-            <button onClick={this.handleClick}>React Button</button>
+            <button onClick={handleClick}>React Button</button>
         </form>
-        );
-    }
+    );
 }
+
+// class Form extends React.Component {
+//     state = {
+//         name: "",
+//         surname: "",
+//         age: ""
+//     }
+//     handleSubmit(e) {
+//         e.preventDefault();
+//     }
+//     handleClick(e) {
+//         console.log("React button");
+//         console.log("handleClick:", e);
+//     }
+//     handleChange = (e) => {
+//         const keyName = e.target.name;
+//         const keyValue = e.target.value;
+
+//         if(keyValue) this.setState({ [keyName] : e.target.value });
+//     }
+//     render() {
+//         return (
+//         <form onSubmit={this.handleSubmit}> 
+//             <div>
+//                 <input name="name" type="text" placeholder="Name" onChange={this.handleChange} />
+//             </div>
+//             <div>
+//                 <input name="surname" type="text" placeholder="Surname" onChange={this.handleChange} />
+//             </div>
+//             <div>
+//                 <input name="age" type="text" placeholder="Age" onChange={this.handleChange} />
+//             </div>
+//             <button onClick={this.handleClick}>React Button</button>
+//         </form>
+//         );
+//     }
+// }
 
 export default Form;
